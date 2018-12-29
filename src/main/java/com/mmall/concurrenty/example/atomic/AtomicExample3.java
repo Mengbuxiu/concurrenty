bb7af861efa6b7477efe5fe86322a3c58e610bbd
milu <15910344021@163.com>
2018-12-29 22:51:22 +0800

    atomic

---
+++ src/main/java/com/mmall/concurrenty/example/atomic/AtomicExample3.java
@@ -0,0 +1,44 @@
+package com.mmall.concurrenty.example.atomic;
+
+import com.mmall.concurrenty.annoations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.LongAdder;
+
+@Slf4j
+@ThreadSafe
+public class AtomicExample3 {
+    private static final int clientTotal = 5000;
+    private static final int threadTotal = 200;
+    private static LongAdder count = new LongAdder();
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("count:{}", count);
+    }
+
+    private static void add() {
+        count.increment();
+        //count.getAndIncrement();
+    }
+}
---
+++ src/main/java/com/mmall/concurrenty/example/atomic/AtomicExample5.java
@@ -0,0 +1,26 @@
+package com.mmall.concurrenty.example.atomic;
+
+import com.mmall.concurrenty.annoations.ThreadSafe;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+import java.util.concurrent.atomic.AtomicReference;
+
+@Slf4j
+@ThreadSafe
+public class AtomicExample5 {
+    private static final AtomicIntegerFieldUpdater<AtomicExample5> updater = AtomicIntegerFieldUpdater.newUpdater(AtomicExample5.class,"count");
+
+    @Getter
+    public volatile int count = 100;
+
+    private static AtomicExample5 example5 = new AtomicExample5();
+
+    public static void main(String[] args) {
+        if (updater.compareAndSet(example5,100,120)) {
+            log.info("update success,{}",example5.getCount());
+        } else
+            log.info("update fail,{}",example5.getCount());
+    }
+}

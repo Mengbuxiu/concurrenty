5722ddf7a7f18552fb3d082a6607a6110739edfe
milu <15910344021@163.com>
2018-12-29 13:28:41 +0800

    Initial commit

---
+++ src/main/java/com/mmall/concurrenty/ConcurrentyApplication.java
@@ -0,0 +1,14 @@
+package com.mmall.concurrenty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrentyApplication.class, args);
+    }
+
+}
+

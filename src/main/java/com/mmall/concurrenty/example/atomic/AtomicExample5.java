94e035251b931f9026497af82535d03937b69589
milu <15910344021@163.com>
2018-12-31 00:04:02 +0800

    atomic

--- src/main/java/com/mmall/concurrenty/example/atomic/AtomicExample5.java
+++ src/main/java/com/mmall/concurrenty/example/atomic/AtomicExample5.java
@@ -10,16 +10,22 @@ import java.util.concurrent.atomic.AtomicReference;
 @Slf4j
 @ThreadSafe
 public class AtomicExample5 {
+    /**
+     * 执行原子性的修改
+     */
     private static final AtomicIntegerFieldUpdater<AtomicExample5> updater = AtomicIntegerFieldUpdater.newUpdater(AtomicExample5.class,"count");
 
     @Getter
     public volatile int count = 100;
 
-    private static AtomicExample5 example5 = new AtomicExample5();
 
     public static void main(String[] args) {
+        AtomicExample5 example5 = new AtomicExample5();
+        if (updater.compareAndSet(example5,110,120)) {
+            log.info("update success 1,{}",example5.getCount());
+        }
         if (updater.compareAndSet(example5,100,120)) {
-            log.info("update success,{}",example5.getCount());
+            log.info("update success 2,{}",example5.getCount());
         } else
             log.info("update fail,{}",example5.getCount());
     }
